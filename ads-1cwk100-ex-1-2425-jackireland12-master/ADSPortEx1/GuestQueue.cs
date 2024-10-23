5e081a8e240dbefefb917692998a8770960d232f
jackireland2 <121182167+jackireland2@users.noreply.github.com>
2024-10-23 01:46:04 +0100

    first

---
+++ ads-1cwk100-ex-1-2425-jackireland12-master/ADSPortEx1/GuestQueue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSPortEx1
+{
+    //Queue implementation for Assessed Exercise 1
+
+    //Hints : 
+    //Use lecture materials from Week 2A
+    // and lab sheet 'Lab 2 (second part): Stacks and Queues' to aid with implementation...
+
+    //Do not delete the current function definitions, just replace the exceptions with the required functionality...
+
+    //Don't forget to properly commit and document your work! 
+    // Commit fairly frequently with proper descriptions, i.e. commit after implementing the enqueue function...
+    
+    //Lastly, don't forget about implementing your UI in 'Form1.cs' too!
+    // See lab material from week 1 to aid with this...
+
+    // - Adam.M 
+
+    class GuestQueue
+    {
+
+        // Functions for EX.1A
+        public GuestQueue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GuestQueue(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(Guest value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guest Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guest Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFull()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Functions for EX.1B
+
+        public void Reverse(int k)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guest GetMostFunds()
+        {
+            throw new NotImplementedException();
+        }
+
+        // See tasksheet for details for EX.1C
+
+
+
+        //Free space, use as necessary to address task requirements... 
+
+
+
+
+        ///hgfjgfg
+
+
+    } // End of class
+}
